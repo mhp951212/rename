6184c27e5c4a9c10c243e9f3363859c9271b2290
meihuipei <824574035@qq.com>
2023-04-19 14:42:26 +0800

    [update]提交预设

--- Form1.cs
+++ Form1.cs
@@ -9,13 +9,82 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace RenameForm
-{
+{   
     public partial class Form1 : Form
-    {
+    {   
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件路径";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                beforImportLab.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件路径";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                afterImportLab.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件路径";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                beforeEportLab.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件路径";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                afterEportLab.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void beforeBtn_Click(object sender, EventArgs e)
+        {
+            //if (beforImportLab.Text == "")
+            //{
+            //    MessageBox.Show("请选择要导入的文件夹路径");
+            //    return;
+            //}
+            //if (beforeEportLab.Text == "")
+            //{
+            //    MessageBox.Show("请选择输出要保存的路径");
+            //    return;
+            //}
+
+
+        }
+
+        private void afterBtn_Click(object sender, EventArgs e)
+        {
+            if (afterImportLab.Text == "")
+            {
+                MessageBox.Show("请选择要导入的文件夹路径");
+                return;
+            }
+            if (afterEportLab.Text == "")
+            {
+                MessageBox.Show("请选择输出要保存的路径");
+                return;
+            }
+        }
     }
 }
